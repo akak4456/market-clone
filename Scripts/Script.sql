347eaf6357f18f69432ffe41e87d6c998ef7bb12
akak35793-oss <akak4456@naver.com>
2026-02-04 11:13:18 +0900

    회원 가입 로직 기능 구현
    프론트엔드 및 백엔드 구현

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,9 +1,10 @@
-CREATE TABLE items (
-	id INTEGER PRIMARY KEY,
-	title TEXT NOT NULL,
-	image BLOB,
-	price INTEGER NOT NULL,
-	description TEXT,
-	place TEXT NOT NULL,
-	insertAt INTEGER NOT NULL
-);
+--CREATE TABLE items (
+--	id INTEGER PRIMARY KEY,
+--	title TEXT NOT NULL,
+--	image BLOB,
+--	price INTEGER NOT NULL,
+--	description TEXT,
+--	place TEXT NOT NULL,
+--	insertAt INTEGER NOT NULL
+--);
+
